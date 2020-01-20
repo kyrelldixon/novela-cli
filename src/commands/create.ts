e8b8f22fae9325afe63d3764de9d96919aa89864
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-01-20 08:46:32 -0600

    feat(command): add simple create command

---
+++ src/commands/create.ts
@@ -0,0 +1,28 @@
+import {Command, flags} from '@oclif/command'
+import * as fs from 'fs'
+
+export default class Create extends Command {
+  static description = 'create a new post'
+
+  static examples = [
+    `$ create-novela-post create
+Kyrell Dixon
+Sam Smith
+`,
+  ]
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    // flag with a value (-n, --name=VALUE)
+    name: flags.string({char: 'n', description: 'name to print'}),
+    // flag with no value (-f, --force)
+    force: flags.boolean({char: 'f'}),
+  }
+
+  static args = [{name: 'file'}]
+
+  async run() {
+    const file = fs.readFileSync('./content/authors/authors.yml', 'utf8')
+    this.log(file)
+  }
+}
