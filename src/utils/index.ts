67cef1f54eadebe69bf69d2eeeb93f85c86c9d67
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-01-20 19:07:24 -0600

    feat: add createPost and helper functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs'
 import * as yaml from 'js-yaml'
-import {Author} from '../types'
+import {Author, Post} from '../types'
 
 export const fetchAuthors = (path: string) => {
   try {
@@ -15,4 +15,30 @@ export const fetchAuthors = (path: string) => {
   } catch (error) {
     throw error
   }
+}
+
+// https://github.com/narative/gatsby-theme-novela/blob/master/%40narative/gatsby-theme-novela/src/utils/index.ts
+const toKebabCase = (str: string): string => {
+  return (str
+  .match(/[A-Z]{2,}(?=[A-Z][a-z]+\d*|\b)|[A-Z]?[a-z]+\d*|[A-Z]|\d+/g) || [])
+  .map(x => x.toLowerCase())
+  .join('-')
+}
+
+const createPostDirectory = (post: Post) => {
+  const formattedTitle = toKebabCase(post.title)
+  const directoryName = `${post.date}-${formattedTitle}`
+  // eslint-disable-next-line no-console
+  console.log(`creating directory ${directoryName}`)
+  // fs.mkdirSync()
+}
+
+const createPostMdxFile = (post: Post) => {
+  // eslint-disable-next-line no-console
+  console.log(`creating post mdx file for ${post.title}`)
+}
+
+export const createPost = (post: Post) => {
+  createPostDirectory(post)
+  createPostMdxFile(post)
 }
