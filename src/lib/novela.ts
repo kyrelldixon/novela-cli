8047faa6a39f842ea122e87774eba2ae8200cd02
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-01-20 18:16:47 -0600

    feat: validate date input

--- src/lib/novela.ts
+++ src/lib/novela.ts
@@ -4,6 +4,17 @@ const excerptValidator = async (input: string) => {
   return input.length < 140 || 'The excerpt should be less than 140 characters'
 }
 
+// from https://stackoverflow.com/questions/18758772/how-do-i-validate-a-date-in-this-format-yyyy-mm-dd-using-jquery
+const dateValidator = async (input: string) => {
+  const errorMessage = 'Date should be a real date formatted as YYYY-MM-DD'
+  const regEx = /^\d{4}-\d{2}-\d{2}$/
+  if (!input.match(regEx)) return errorMessage  // Invalid format
+  const d = new Date(input)
+  const dNum = d.getTime()
+  if (!dNum && dNum !== 0) return errorMessage // NaN value, Invalid date
+  return (d.toISOString().slice(0, 10) === input) || errorMessage
+}
+
 export const makePostQuestions = (authors: Author[]) => [
   {
     name: 'title',
@@ -19,6 +30,7 @@ export const makePostQuestions = (authors: Author[]) => [
   {
     name: 'date',
     message: 'Date (YYYY-MM-DD)',
+    validate: dateValidator,
   },
   {
     name: 'excerpt',
